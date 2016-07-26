8ac8d7a1f292df08766331852523b1a8a7f76857
chasetrox <chasetrox@gmail.com>
2016-07-26 15:16:45 -0400

    JSON-ified station data

--- mbta/gmaps-script.js
+++ mbta/gmaps-script.js
@@ -10,28 +10,28 @@ var myOptions = {
 
 // Station geolocations organized into an unordered array of associative objects
 var stations = {
-'South Station' : { 'lat': 42.352271,   'lng': -71.05524200000001,  'next': 'Broadway'},
-'Andrew'        : { 'lat': 42.330154,   'lng':          -71.057655, 'next': 'JFK/UMass'},
-'Porter Square' : { 'lat':     42.3884, 'lng':  -71.11914899999999, 'next': 'Harvard Square'},
-'Harvard Square': { 'lat':   42.373362, 'lng':          -71.118956, 'next': 'Central Square'},
-'JFK/UMass'     : { 'lat':   42.320685, 'lng':          -71.052391, 'next': 'North Quincy'},
-'Savin Hill'    : { 'lat' :    42.31129,'lng':          -71.053331, 'next': 'JFK/UMass'},
-'Park Street'   : { 'lat': 42.35639457, 'lng':         -71.0624242, 'next': 'Downtown Crossing'},
-'Broadway'      : { 'lat':   42.342622, 'lng':          -71.056967, 'next': 'Andrew'},
-'North Quincy'  : { 'lat' :   42.275275,'lng':          -71.029583, 'next': 'Wollaston'},
-'Shawmut'       : { 'lat': 42.29312583, 'lng':  -71.06573796000001, 'next': 'Fields Corner'},
-'Davis'         : { 'lat':    42.39674, 'lng':          -71.121815, 'next': 'Porter Square'},
-'Alewife'       : { 'lat' :   42.395428, 'lng':         -71.142483, 'next': 'Davis'},
-'Kendall/MIT'   : { 'lat': 42.36249079, 'lng':        -71.08617653, 'next': 'Charles/MGH'},
-'Charles/MGH'   : { 'lat':   42.361166, 'lng':          -71.070628, 'next': 'Park Street'},
-'Downtown Crossing':{'lat':42.355518,   'lng':          -71.060225, 'next': 'South Station'},
-'Quincy Center' : { 'lat':   42.251809, 'lng':          -71.005409, 'next': 'Quincy Adams'},
-'Quincy Adams'  : { 'lat':   42.233391, 'lng':          -71.007153, 'next': 'Braintree'},
-'Ashmont'       : { 'lat':   42.284652, 'lng':  -71.06448899999999, 'next': 'Shawmut'},
-'Wollaston'     : { 'lat':  42.2665139, 'lng':         -71.0203369, 'next': 'Quincy Center'},
-'Fields Corner': { 'lat':   42.300093, 'lng':          -71.061667, 'next': 'Savin Hill'},
-'Central Square': { 'lat':   42.365486, 'lng':          -71.103802, 'next': 'Kendall/MIT'},
-'Braintree'     : { 'lat':  42.2078543, 'lng':         -71.0011385, 'next': 'Ashmont'}
+'South Station' : { lat: 42.352271,   lng: -71.05524200000001,  next: 'Broadway'},
+'Andrew'        : { lat: 42.330154,   lng:          -71.057655, next: 'JFK/UMass'},
+'Porter Square' : { lat:     42.3884, lng:  -71.11914899999999, next: 'Harvard Square'},
+'Harvard Square': { lat:   42.373362, lng:          -71.118956, next: 'Central Square'},
+'JFK/UMass'     : { lat:   42.320685, lng:          -71.052391, next: 'North Quincy'},
+'Savin Hill'    : { lat :    42.31129,lng:          -71.053331, next: 'JFK/UMass'},
+'Park Street'   : { lat: 42.35639457, lng:         -71.0624242, next: 'Downtown Crossing'},
+'Broadway'      : { lat:   42.342622, lng:          -71.056967, next: 'Andrew'},
+'North Quincy'  : { lat :   42.275275,lng:          -71.029583, next: 'Wollaston'},
+'Shawmut'       : { lat: 42.29312583, lng:  -71.06573796000001, next: 'Fields Corner'},
+'Davis'         : { lat:    42.39674, lng:          -71.121815, next: 'Porter Square'},
+'Alewife'       : { lat :   42.395428, lng:         -71.142483, next: 'Davis'},
+'Kendall/MIT'   : { lat: 42.36249079, lng:        -71.08617653, next: 'Charles/MGH'},
+'Charles/MGH'   : { lat:   42.361166, lng:          -71.070628, next: 'Park Street'},
+'Downtown Crossing':{lat:42.355518,   lng:          -71.060225, next: 'South Station'},
+'Quincy Center' : { lat:   42.251809, lng:          -71.005409, next: 'Quincy Adams'},
+'Quincy Adams'  : { lat:   42.233391, lng:          -71.007153, next: 'Braintree'},
+'Ashmont'       : { lat:   42.284652, lng:  -71.06448899999999, next: 'Shawmut'},
+'Wollaston'     : { lat:  42.2665139, lng:         -71.0203369, next: 'Quincy Center'},
+'Fields Corner': { lat:   42.300093, lng:          -71.061667, next: 'Savin Hill'},
+'Central Square': { lat:   42.365486, lng:          -71.103802, next: 'Kendall/MIT'},
+'Braintree'     : { lat:  42.2078543, lng:         -71.0011385, next: 'Ashmont'}
 };
 
 /* Client function, initializes map, draws markers/red line */
@@ -41,6 +41,7 @@ function init() {
         place_markers(map);
         draw_red_line(map);
         var user = mark_user(map);
+
 }
 
 /* Iterates through stops and places custom icon'ed markers */
@@ -52,8 +53,8 @@ function place_markers(map) {
         //Iterates through stops, draws marker at lng/lat on the map
         for (var stop in stations) {
                 if (stations.hasOwnProperty(stop)) {
-                        var lat = stations[stop]['lat'];
-                        var lng = stations[stop]['lng'];
+                        var lat = stations[stop].lat;
+                        var lng = stations[stop].lng;
                         var pos = new google.maps.LatLng(lat, lng);
                         marker = new google.maps.Marker({
                                         icon: icon,
@@ -76,11 +77,11 @@ function draw_red_line(map) {
 
                 /* Array with coordinates for path b/w two stops */
                 var pathCoords = [];
-                pathCoords.push({lat: stop['lat'], lng: stop['lng']})
-                pathCoords.push({lat: stations[stop['next']]['lat'],
-                           lng: stations[stop['next']]['lng']});
+                pathCoords.push({lat: stop.lat, lng: stop.lng})
+                pathCoords.push({lat: stations[stop.next].lat,
+                           lng: stations[stop.next].lng});
 
-                if (stop['next'] != 'Ashmont') {
+                if (stop.next != 'Ashmont') {
                         var line = new google.maps.Polyline({
                             path: pathCoords,
                             strokeColor: '#FF0000',
@@ -91,19 +92,61 @@ function draw_red_line(map) {
                 }
 
                 if (curr == 'Savin Hill') finished = true;
-                curr = stop['next'];
+                curr = stop.next;
         }
 }
 
 function mark_user(map) {
         if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
-                        var pos = new google.maps.LatLng(position.coords.latitude,
-                                                        position.coords.longitude);
-			marker.setMap(new google.maps.Marker({position: pos}));
+                        var pos = {lat: position.coords.latitude,
+                                   lng: position.coords.longitude};
+                        var marker = new google.maps.Marker({position: pos});
+			marker.setMap(map);
+                        closest_station = create_infobox(marker, pos);
                         return pos;
                 });
         } else {
-                console.log("Your browser does not support this feature.")
+                console.log("Your browser does not support this feature.");
         }
+}
+
+function create_infobox(marker, pos) {
+        closest_station = "";
+        var distance = NaN;
+
+        for (stop in stations) {
+                st_pos = {lat: stations[stop].lat, lng: stations[stop].lng};
+                var d = haversine(pos, st_pos);
+                if (d < distance || distance == NaN) {
+                        distance = d;
+                        closest_station = stop;
+                }
+        }
+
+
+}
+
+//Credit goes to talkol from Stack Overflow
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+
+function haversine(pos1, pos2) {
+        console.log(pos1);
+        var lat1 = pos1.lat, lng1 = pos1.lng;
+        var lat2 = pos2.lat, lng2 = pos2.lng;
+
+        var R = 6371; // km
+        //has a problem with the .toRad() method below.
+        var x1 = lat2-lat1;
+        var dLat = x1.toRad();
+        var x2 = lon2-lon1;
+        var dLon = x2.toRad();
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        var d = R * c;
 }
